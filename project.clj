40449162f914851bfecd92ec0d960bef1e26e387
shark8me <kiran.karkera@gmail.com>
2019-06-18 19:20:56 +0800

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject chaturpandit "0.1.0-SNAPSHOT"
+(defproject chaturpandit "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.520"]
                  [reagent "0.8.1"]
